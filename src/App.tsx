807fe7dc4c6227884d9c11d8b958d9faaeec61c3
Caio Roberto Ramos da Silva <caio@Caios-MacBook-Air.local>
2022-08-28 19:59:12 -0300

    Added react navigation

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import {Text, View} from 'react-native';
+
+import Navigator from './navigation';
+import React from 'react';
+
+const App = () => {
+  return <Navigator />;
+};
+
+export default App;
