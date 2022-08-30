23f90d49f5e2aa4a24d0c59a2d0651c39dfc1873
Caio Roberto Ramos da Silva <caio@Caios-MacBook-Air.local>
2022-08-29 23:08:29 -0300

    Finished Details page

--- src/screens/Details/Details.tsx
+++ src/screens/Details/Details.tsx
@@ -1,6 +1,6 @@
 import {ActivityIndicator, Pressable, Text, View} from 'react-native';
 import {Episode, ShowDetailsWithEpisodesResponse} from '../../services/types';
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 
 import FastImage from 'react-native-fast-image';
 import {FlashList} from '@shopify/flash-list';
@@ -8,21 +8,40 @@ import {Header} from '../../components';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../../navigation/types';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {createStyles} from './styles';
 import {getShowDetailsWithEpisodes} from '../../services/shows';
-import {useNavigation} from '@react-navigation/native';
+// import moment from 'moment'
+import {useTheme} from '../../contexts/ThemeContext';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Details'>;
 
-export const Details = ({route}: Props) => {
+export const Details = ({route, navigation}: Props) => {
   const {showId} = route.params;
 
-  const navigation = useNavigation();
+  const {theme} = useTheme();
+  const styles = createStyles(theme);
 
   const [data, setData] = useState<ShowDetailsWithEpisodesResponse | null>(
     null,
   );
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+
+  const seasons = useMemo(() => {
+    let response: (number | Episode)[] = [];
+    if (data) {
+      for (let i = 0; i < data._embedded.episodes.length; i++) {
+        if (!response.includes(data._embedded.episodes[i].season)) {
+          response.push(data._embedded.episodes[i].season);
+          response.push(data._embedded.episodes[i]);
+        } else {
+          response.push(data._embedded.episodes[i]);
+        }
+      }
+    }
+
+    return response;
+  }, [data]);
 
   const fetchShowDetails = async () => {
     setLoading(true);
@@ -38,20 +57,41 @@ export const Details = ({route}: Props) => {
 
   useEffect(() => {
     fetchShowDetails();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const renderItem = ({item: episode}: {item: Episode}) => {
+  const stickyHeaderIndices = seasons
+    .map((item, index) => {
+      if (typeof item === 'number') {
+        return index;
+      } else {
+        return null;
+      }
+    })
+    .filter(item => item !== null) as number[];
+
+  const renderItem = ({item: episode}: {item: Episode | number}) => {
+    if (typeof episode === 'number') {
+      return (
+        <View style={styles.sectionContainer}>
+          <Text style={styles.sectionText}>
+            Season {String(episode).padStart(2, '0')}
+          </Text>
+        </View>
+      );
+    }
+
     return (
       <Pressable
-        style={{flexDirection: 'row', margin: 10}}
+        style={styles.episode}
         onPress={() => navigation.navigate('Episode', {episode})}>
         <FastImage
           source={{uri: episode.image.medium}}
-          style={{height: 50, width: 50}}
+          style={styles.episodeImage}
         />
-        <View style={{paddingLeft: 20}}>
-          <Text>{episode.name}</Text>
-          <Text>
+        <View style={styles.episodeInfo}>
+          <Text style={styles.episodeName}>{episode.name}</Text>
+          <Text style={styles.episodeNumber}>
             S{episode.season}E{episode.number}
           </Text>
         </View>
@@ -60,19 +100,33 @@ export const Details = ({route}: Props) => {
   };
 
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={styles.container}>
       <Header title={data?.name || 'Details'} onBack={navigation.goBack} />
       {loading && <ActivityIndicator size={'large'} />}
       {error ? (
         <Text>{err.message}</Text>
       ) : (
         <>
-          <Text>{data?.name}</Text>
-          <Text>{data?.rating.average}</Text>
+          <FastImage
+            source={{uri: data?.image.original}}
+            style={styles.poster}
+            resizeMode={FastImage.resizeMode.contain}
+          />
+          <View style={styles.infoContainer}>
+            <Text style={styles.genres}>Genres: {data?.genres.join(', ')}</Text>
+            <Text style={styles.summaryTitle}>Summary:</Text>
+            <Text style={styles.summary}>{data?.summary}</Text>
+            {/* <Text>Airs on: {moment(data?.schedule.days)}</Text> */}
+          </View>
           <View style={{flex: 1}}>
             <FlashList
-              data={data?._embedded?.episodes}
+              data={seasons}
               renderItem={renderItem}
+              getItemType={type =>
+                typeof type === 'number' ? 'sectionHeader' : 'row'
+              }
+              estimatedItemSize={70}
+              stickyHeaderIndices={stickyHeaderIndices}
             />
           </View>
         </>
--- src/screens/Details/styles.ts
+++ src/screens/Details/styles.ts
@@ -1,3 +1,61 @@
 import {StyleSheet} from 'react-native';
+import {ThemeType} from '../../assets/theme';
 
-export const styles = StyleSheet.create({});
+export const createStyles = (theme: ThemeType) =>
+  StyleSheet.create({
+    sectionContainer: {
+      backgroundColor: theme.colors.background,
+      padding: 5,
+    },
+    sectionText: {
+      color: theme.colors.text,
+      fontSize: 16,
+      fontWeight: '600',
+    },
+    infoContainer: {
+      padding: 15,
+    },
+    poster: {
+      height: 150,
+      width: '100%',
+      backgroundColor: theme.colors.background,
+    },
+    genres: {
+      color: theme.colors.text,
+      fontSize: 18,
+      fontWeight: '600',
+      paddingVertical: 5,
+    },
+    summaryTitle: {
+      color: theme.colors.text,
+      fontSize: 24,
+      fontWeight: 'bold',
+      paddingVertical: 5,
+    },
+    summary: {
+      color: theme.colors.text,
+      fontSize: 18,
+      paddingVertical: 5,
+    },
+    episode: {
+      flexDirection: 'row',
+      margin: 10,
+    },
+    episodeInfo: {
+      paddingLeft: 20,
+    },
+    episodeName: {
+      color: theme.colors.text,
+    },
+    episodeNumber: {
+      color: theme.colors.text,
+    },
+    episodeImage: {
+      height: 50,
+      width: 50,
+    },
+    container: {
+      flex: 1,
+      backgroundColor: theme.colors.background,
+    },
+  });
