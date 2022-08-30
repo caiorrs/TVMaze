f2f53edcc0afb8970938e9caf062550120f786b5
Caio Roberto Ramos da Silva <caio@Caios-MacBook-Air.local>
2022-08-29 23:16:09 -0300

    Finished episode screen

--- src/screens/Episode/Episode.tsx
+++ src/screens/Episode/Episode.tsx
@@ -1,22 +1,37 @@
-import {FlashList} from '@shopify/flash-list';
+import {Text, View} from 'react-native';
+
+import FastImage from 'react-native-fast-image';
 import {Header} from '../../components';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React from 'react';
 import {RootStackParamList} from '../../navigation/types';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {Text} from 'react-native';
+import {createStyles} from './styles';
+import {useTheme} from '../../contexts/ThemeContext';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Episode'>;
 
 export const Episode = ({navigation, route}: Props) => {
   const {episode: episodeDetails} = route.params;
 
+  const {theme} = useTheme();
+  const styles = createStyles(theme);
+
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={styles.container}>
       <Header title={episodeDetails.name} onBack={navigation.goBack} />
-      <Text>Episode</Text>
-      <Text>{episodeDetails.name}</Text>
-      {/* <FlashList /> */}
+      <FastImage
+        source={{uri: episodeDetails.image.original}}
+        style={styles.poster}
+        resizeMode={FastImage.resizeMode.contain}
+      />
+      <View style={styles.infoContainer}>
+        <Text style={styles.episodeName}>Episode: {episodeDetails.name}</Text>
+        <Text style={styles.season}>Season: {episodeDetails.season}</Text>
+        <Text style={styles.number}>Number: {episodeDetails.number}</Text>
+        <Text style={styles.summaryTitle}>Summary:</Text>
+        <Text style={styles.summary}>{episodeDetails.summary}</Text>
+      </View>
     </SafeAreaView>
   );
 };
--- src/screens/Episode/styles.ts
+++ src/screens/Episode/styles.ts
@@ -1,3 +1,39 @@
 import {StyleSheet} from 'react-native';
+import {ThemeType} from '../../assets/theme';
 
-export const styles = StyleSheet.create({});
+export const createStyles = (theme: ThemeType) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: theme.colors.background,
+    },
+    poster: {
+      height: 200,
+      width: '100%',
+    },
+    infoContainer: {
+      padding: 15,
+    },
+    episodeName: {
+      fontSize: 24,
+      color: theme.colors.text,
+      fontWeight: 'bold',
+    },
+    season: {
+      fontSize: 18,
+      color: theme.colors.text,
+    },
+    number: {
+      fontSize: 18,
+      color: theme.colors.text,
+    },
+    summaryTitle: {
+      fontSize: 22,
+      color: theme.colors.text,
+      fontWeight: 'bold',
+    },
+    summary: {
+      fontSize: 18,
+      color: theme.colors.text,
+    },
+  });
